4ccedfa94c2cfd9bb128fd12afca8a6507f0a407
menmhou <52415472+menmhou@users.noreply.github.com>
2020-08-20 13:32:35 +0100

    Added Workout controller

---
+++ DIYHIIT.API/Controllers/WorkoutController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DIYHIIT.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DIYHIIT.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WorkoutController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public WorkoutController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext ??
+                throw new ArgumentNullException(nameof(appDbContext));
+        }
+
+        [HttpGet]
+        [Route("workouts")]
+        public async Task<IActionResult> GetWorkouts()
+        {
+            var items = await _appDbContext.Workouts
+                                           .OrderBy(w => w.ID)
+                                           .ToListAsync();
+
+            return Ok(items);
+        }
+    }
+}
