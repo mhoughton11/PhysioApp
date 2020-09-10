140faed403bea7f6b8f8f65c765a41d7c804d40e
menmhou <52415472+menmhou@users.noreply.github.com>
2020-09-10 21:28:44 +0100

    Added more exercises

--- DIYHIIT.Library/Models/Exercise.cs
+++ DIYHIIT.Library/Models/Exercise.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Diagnostics;
+using System.Windows.Input;
 using DIYHIIT.Library.Contracts;
 
 namespace DIYHIIT.Library.Models
